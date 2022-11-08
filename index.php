0892157dca7250ff93ab2fa4e0e59b119c7626fe
Diego Guadalupe <9005567@alumnos.ufv.es>
2022-11-08 12:19:17 +0100

    Commit de los archivos para GitHub

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Formulario de inicio de sesión con PHP & MySQL</title>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Flat Login Form Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
+<!--google fonts-->
+</head>
+<body>
+<!--header start here-->
+<h1>Formulario de inicio de sesión</h1>
+<div class="header agile">
+	<div class="wrap">
+		<div class="login-main wthree">
+			<div class="login">
+			<h3>Iniciar sesión</h3>
+			<form action="login.php" method="post">
+				<input type="text" placeholder="Usuario" required="" name="username" required>
+				<input type="password" placeholder="Contraseña" name="password" required>
+				<input name="submit" type="submit" value="Ingresar">
+			</form>
+			<div class="clear"> </div>
+				<span><?php echo $error; ?></span>
+			</div>
+			
+			
+		</div>
+	</div>
+</div>
+<!--header end here-->
+
+</body>
+</html>
